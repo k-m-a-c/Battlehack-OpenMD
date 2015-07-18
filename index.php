b4392ae1e6f6c4fa834019f1d7291850af82753c
Chris Cates <codeviolet@chriscates.ca>
2015-07-18 18:54:07 -0400

    Bug fixing

--- index.php
+++ index.php
@@ -38,14 +38,9 @@ $app->get(
 
     <script src="https://js.braintreegateway.com/v2/braintree.js"></script>
     <script>
+  var clientToken = "$clientToken";
 
-    var braintree = Braintree.create('MIIBCgKCAQEAuZeCmRto9EPrR1l0lp/mF8a4bchRGobavGLBxSKll+kiMf7PO+qqKifu+PfyADyJxoo/Qwfo8uuU0ZXo5XO1MQP1EZDFuG53Emo+Hz2njXa+kRsJ21PTxsTjyPvOOA5Jt2aKx9oF3HWycM/LOJBG9yuMK0OjykOmKhbCnrjQ0KZ1pHpIbSm45KgL3xaivWCi+WHESS82TMYe2WLN9MDzt1ntrRJanET/qanvgReBOZKwNj8Vlre2xT30wDRCkjBvKPSdTJEtgySykp4AHs4coOXHsnjf9JZpdlIX3HXjPMbVXUKpoFHL8b1BMf4BlYON7+oIyuvufU2M5SKGce3d9QIDAQAB');
-    // We generated a client token for you so you can test out this code
-    // immediately. In a production-ready integration, you will need to
-    // generate a client token on your server (see section below).
-    var clientToken = "$clientToken";
-
-    Braintree.setup(clientToken, "dropin", {
+    braintree.setup(clientToken, "dropin", {
       container: "payment-form"
     });
     </script>
