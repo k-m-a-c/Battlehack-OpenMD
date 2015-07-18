9a5c8be23007a8863df3b2bd5d1906355109c6d2
Chris Cates <codeviolet@chriscates.ca>
2015-07-18 15:22:39 -0400

    test api call

--- index.php
+++ index.php
@@ -16,12 +16,41 @@ require('pages/homepage.php');
 $app->get(
   '/api/new/doctor', function() {
     require('connect.php');
+    global $app;
+		$request = $app->request();
+		$data = $request->params();
 
+    $email = $data['email'];
+    /*
+    $password = $data['password'];
+    $password2 = $data['confirm_password'];
+    $name = $data['name'];
+    $specialty = $data['specialty'];
+    $location = $data['location'];
+    $cpso = $data['cpso'];
+    */
+
+    if (strlen($email, "@") < 0) {
+      $response = array(
+        "response":"error",
+        "message":"invalid email"
+      )
+      echo json_encode($response);
+      exit;
+    }
   }
 );
 
-echo <<<HTML
-
+$app->get(
+  '/', function() {
+    echo <<<HTML
+    <form action="/api/new/doctor" method="POST">
+      <input type="text" name="email">
+      <input type="submit" value="Submit">
+    </form>
 HTML;
+  }
+);
+
 
 $app->run();
