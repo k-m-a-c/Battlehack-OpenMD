de339485356cdc1c96a8c5f3e5861e8985531aef
Chris Cates <codeviolet@chriscates.ca>
2015-07-18 23:06:09 -0400

    updated braintree token

--- api/check.bt.token.php
+++ api/check.bt.token.php
@@ -1,6 +1,7 @@
 <?php
 $app->get(
   '/api/check/patient/payed', function() {
+    require('connect.php');
     if (isset($_SESSION['user_id'])) {
       $userId = $_SESSION['user_id'];
       foreach($db->query("SELECT * FROM patients WHERE id = '$userId'") as $row) {
