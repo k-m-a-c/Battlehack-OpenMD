f4c30e0fc6d9a9ab620ce813b0e280ca59d7e51b
Chris Cates <codeviolet@chriscates.ca>
2015-07-19 03:39:33 -0400

    REST API calls in doctor.home.php

--- pages/doctor.home.php
+++ pages/doctor.home.php
@@ -1,29 +1,27 @@
 <?php
-$app->get('/doctor/home', function() {
+$app->get('api/doctor/home', function() {
   require('connect.php');
   if ($_SESSION['user_type'] != "doctor") {
     global $app;
     $app->redirect('/');
   }
-  $doctorId = $_SESSION['user_id'];
-  $html = "<a href='/logout'>Logout</a>";
-  $html .= "<table style='border: 1;'>";
+  $data = array();
   foreach($db->query("SELECT * FROM patients") as $row) {
-    $html .= <<<HTML
-    <tr>
-      <td><a href="/patient/u/{$row['id']}">{$row['name']}</a></td>
-      <td>{$row['city']}, {$row['country']}</td>
-      <td>{$row['healthcard']}</td>
-      <td><a href="/api/doctor/add/patient/{$row['id']}">Add Patient</a></td>
-    </tr>
-HTML;
+    $d = array(
+      'patient_profile_link' => "/patient/u/".$row['id'],
+      'name' => $row['name'],
+      'city' => $row['city'],
+      'country' => $row['country'],
+      'healthcard' => $row['healthcard'],
+      'request_patient_link' => "/api/doctor/add/patient/".$row['id']
+    );
+    array_push($data,$d);
   }
-  $html .= "</table>";
 
-  echo $html;
+  echo json_encode($data);
 });
 
-$app->get('/doctor/requests', function() {
+$app->get('api/doctor/requests', function() {
   require('connect.php');
   if ($_SESSION['user_type'] != "doctor") {
     global $app;
@@ -31,24 +29,23 @@ $app->get('/doctor/requests', function() {
   }
   $doctorId = $_SESSION['user_id'];
 
-  $html = "<table style='border: 1;'>";
+  $data = array();
 
   foreach($db->query("SELECT * FROM patients
   JOIN patients_wanting_doctors ON patients.id = patients_wanting_doctors.patientId
   WHERE patients_wanting_doctors.doctorId = '$doctorId'
   GROUP BY patients.id") as $row) {
-    $html .= <<<HTML
-    <tr>
-      <td><a href='/patient/u/{$row['id']}'>{$row['name']}</a></td>
-      <td>{$row['city']}, {$row['country']}</td>
-      <td>{$row['healthcard']}</td>
-      <td><a href="/accept/patient/{$row['id']}">Accept Patient</a></td>
-    </tr>
-HTML;
+    $d = array(
+      "patient_profile_link" => '/patient/u/'.$row['id'],
+      'name' => $row['name'],
+      'city' => $row['city'],
+      'country' => $row['country'],
+      'healthcard' => $row['healthcard'],
+      'accept_patient_link' => "/accept/patient/".$row['id']
+    );
+    array_push($data,$d);
   }
 
-  $html .= "</table>";
-
-  echo $html;
+  echo json_encode($data);
 });
 ?>
